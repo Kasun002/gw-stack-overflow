32be12a5da3f6195a478ac880f0f155926edfc17
Kasun Abaywardana <abaywardanakasun@gmail.com>
2023-04-29 15:25:47 +0800

    Question Form Added

--- src/pages/question/Question.tsx
+++ src/pages/question/Question.tsx
@@ -1,47 +1,81 @@
 import { useState } from 'react';
-import './Question.css';
-import Header from '../../components/header/Header';
 import { Question } from '../../utils/Interfaces';
-
+import './Question.css';
+import { Button, Form, Input, InputNumber, Select } from 'antd';
+import { Option } from 'antd/es/mentions';
+import { TagsOptions } from '../../utils/Options';
 
 const QuestionPage = () => {
-    const [formData, setFormData] = useState<Question>({} as Question);
-    const [errorMessage, setErrorMessage] = useState('');
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        const { name, value } = e.target;
-        setFormData(prevFormData => ({ ...prevFormData, [name]: value }));
+
+    const layout = {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 16 },
     };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        if (!formData.title || !formData.body) {
-            setErrorMessage('Please enter a title and a body.');
-            return;
-        }
+    const validateMessages = {
+        required: '${label} is required!',
     };
+
+    const onFinish = (values: any) => {
+        //
+    };
+
+    const selectTag = (event: string) => {
+        //
+    }
+
 
     return (
-        <div className="container">
-            <Header />
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Title:
-                    <input type="text" name="title" value={formData.title} onChange={handleChange} />
-                </label>
-                <label>
-                    Body:
-                    <textarea name="body" value={formData.body} onChange={handleChange} />
-                </label>
-                <label>
-                    Tags:
-                    <input type="text" name="tags" value={formData.tags} onChange={handleChange} />
-                </label>
-                {errorMessage && <div className="error-message">{errorMessage}</div>}
-                <button type="submit" disabled={!formData.title || !formData.body}>
-                    Post Your Question
-                </button>
-            </form>
+        <div className='container'>
+            <div className='header header__img'>
+                <div className='header__top'>
+                    <h1>Ask a public question</h1>
+                </div>
+            </div>
+            <div className='card'>
+                <Form
+                    {...layout}
+                    name="nest-messages"
+                    style={{ maxWidth: 'auto' }}
+                    validateMessages={validateMessages}
+                >
+                    <label>Title</label>
+                    <div>Be specific and imagine you’re asking a question to another person</div>
+                    <Form.Item name={['question', 'title']} rules={[{ required: true }]}>
+                        <Input style={{display:'flex' }}/>
+                    </Form.Item>
+                    <label>Body</label>
+                    <div>Include all the information someone would need to answer your question</div>
+                    <Form.Item name={['question', 'body']} rules={[{ required: true }]}>
+                        <Input.TextArea />
+                    </Form.Item>
+                    <label>Tags</label>
+                    <div>Add up to 5 tags to describe what your question is about</div>
+                    <Form.Item name={['question', 'tags']}>
+                        <Select
+                            mode="tags"
+                            showSearch
+                            allowClear
+                            optionLabelProp="children"
+                            onChange={selectTag}
+                            style={{ width: 200 }}
+                            placeholder="Select a person"
+                        >
+                            {TagsOptions.map((option) => {
+                                return (<Option key={option} value={option}>
+                                    {option}
+                                </Option>)
+                            })}
+                        </Select>
+                    </Form.Item>
+                </Form>
+            </div>
+            <div style={{width: "70%", padding:"8px 0px"}}>
+                <Button className="sof-button" type="primary" htmlType="submit" onClick={onFinish}>
+                    Post your question
+                </Button>
+            </div>
         </div>
     );
 }
---
+++ src/utils/Options.ts
@@ -0,0 +1,16 @@
+const TagsOptions: Array<string> = [
+    'node.js', 'jquery', 'php', 'c#', 'vue.js', 'mysql', 'react-native', 'java',
+    'android', 'c++', 'ios', 'laravel', 'django', 'ruby-on-rails', 'swift',
+    'webpack', 'amazon-web-services', 'git', 'docker', 'mongodb', 'sql', 'firebase',
+    'flutter', 'apache-spark', 'kubernetes', 'tensorflow', 'numpy', 'pandas',
+    'machine-learning', 'deep-learning', 'neural-network', 'data-science',
+    'bigdata', 'blockchain', 'ethereum', 'solidity', 'cryptocurrency',
+    'artificial-intelligence', 'azure', 'google-cloud-platform', 'devops',
+    'continuous-integration', 'continuous-deployment', 'testing', 'jest', 'mocha',
+    'selenium', 'react-testing-library', 'unit-testing', 'e2e-testing', 'jestjs',
+    'performance', 'security', 'webpack4', 'babeljs', 'ecmascript-6', 'npm',
+    'yarn', 'webpack-dev-server', 'webpack-plugins', 'webpack-loader',
+    'react-router', 'react-redux',
+];
+
+export {TagsOptions};
