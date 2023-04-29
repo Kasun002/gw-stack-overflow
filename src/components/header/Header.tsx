bccfd6865ee840a6378fafa27099fde7acd16e0c
Kasun Abaywardana <abaywardanakasun@gmail.com>
2023-04-29 11:18:58 +0800

    Header component rearrange with styles

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './Header.css';
+import { useNavigate } from 'react-router-dom';
+import { AppContext } from '../../context/AppContext';
 
 const Header = () => {
+    const contextData = useContext(AppContext);
+
+    const navigate = useNavigate();
+
+    const askQuestion = () => {
+        navigate('/question');
+    }
+
     return (
-        <>
-            <div>
+        <div className='header'>
+            <div className='header__top'>
                 <h1>All Questions</h1>
-                <button>
+                <button onClick={askQuestion}>
                     Ask Question
                 </button>
             </div>
-            <div>
-                <div>
-                    23,683,186
+            <div className='header__bottom'>
+                <div className='text'>
+                {contextData.questions.length}&nbsp;
                     questions
                 </div>
-                <div>
-                    <div>
+                <div className='filter'>
+                    <div className='list'>
                         <a href="">Newest</a>
                         <a href="">Active</a>
-                        <a href=""> <div>Bountied</div> <div>245</div></a>
+                        <a href=""><div className='tag-wrapper'>Bountied <div className='tag'>245</div></div></a>
                         <a href="">Unanswered</a>
                         <button>More</button>
                     </div>
                     <button>Filter</button>
                 </div>
             </div>
-        </>
-
-
+        </div>
     );
 }
 
